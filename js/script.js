a054bbd0f6c9c07f8373e7c08d95412e51a6e371
ariel513 <astvishor@gmail.com>
2025-11-10 09:00:53 -0600

    c

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 var bottomText = document.getElementById('textoutput');
 bottomText.innerHTML = "Show Results Here";
+
 var js = document.getElementById('entrybutton');
 js.addEventListener("click", init);
 
 
 function init() {
 //add your javascrip between these two lines of code
-  bottomText.innerHTML = "Show Results Here";
   var text = document.getElementById('entryinput');
   alert('Ariel Tvishor: ' + text.value);
   bottomText.innerHTML = text.value;
 }
+
+function loading() {
+  bottomText.innerHTML = "Show Results Here";
+}
+
+
+window.addEventListener('load', loading);
