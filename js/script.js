dc77d16a5ba6448886b6a7503d7c294c28a7a7c8
ariel513 <astvishor@gmail.com>
2025-11-10 08:47:42 -0600

    java changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-function init(){
+var bottomText = document.getElementById('textoutput');
+bottomText.innerHTML = "Show Results Here";
+var js = document.getElementById('entrybutton');
+js.addEventListener("click", init);
+
+
+function init() {
 //add your javascrip between these two lines of code
- 
+  var text = document.getElementById('entryinput');
+  alert('Ariel Tvishor: ' + text.value);
+  bottomText.innerHTML = text.value;
+}
 
 
 
